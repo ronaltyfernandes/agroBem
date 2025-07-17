5517fc8703bb83687f6d23c90d5fc58fd048f38b
ronalty fernandes <ronaltyfernandess@gmail.com>
2025-07-17 02:31:53 -0300

    adddd.

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -10,10 +10,10 @@ function Welcome() {
   return (
     <section
       id="welcome"
-      className={`bg-cover h-full bg-cente pb-12 pt-24 lg:0 pt-8 lg-pt-0`}
+      className={`bg-cover lg:h-full bg-cente pb-12 pt-24 lg:0 pt-8 lg-pt-0`}
       style={{ backgroundImage: `url(${FarmHeaderImage})` }}
     >
-      <div className="justify-center flex flex-col h-full text-white">
+      <div className="justify-center flex flex-col lg:h-full text-white">
         <Card className="w-10/12 md:w-4/8 p-8 mx-auto">
           <p className="text-lg text-gray-400 text-justify">Bem vindo à vida saudável,</p>
           <h1 className="text-5xl font-bold mb-16 mt-4" style={{ color: colors.primary }}>
